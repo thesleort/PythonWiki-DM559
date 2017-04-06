baa9effba49cf9829f5da1e45ceaab2973be5804
Mark Jervelund <mark@jervelund.com>
2017-04-06 14:36:19 +0200

    updated some files.

--- functions/lib.py
+++ functions/lib.py
@@ -18,11 +18,11 @@ def mulVec(a, b): return [a[i] * b[i] for i in range(len(b))]
 def divVec(a, b): return [a[i] / b[i] for i in range(len(b))]
 '''
 
+
 def numpySum(a, b):
     matrixa = np.matrix(a)
     matrixb = np.matrix(b)
     return matrixa + matrixb
-
 
 
 def numpysub(a, b):
@@ -30,66 +30,67 @@ def numpysub(a, b):
     matrixb = np.matrix(b)
     return matrixa - matrixb
 
+
 def numpymul(a, b):
     matrixa = np.matrix(a)
     matrixb = np.matrix(b)
     return matrixa * matrixb
+
 
 def numpydiv(a, b):
     matrixa = np.matrix(a)
     matrixb = np.matrix(b)
     return matrixa / matrixb
 
-def numpydot(a,b):
-    matrixa = np.matrix(a)
-    matrixb = np.matrix(b)
-    return np.dot(matrixa,matrixb);
 
-def numpycross(a,b):
+def numpydot(a, b):
     matrixa = np.matrix(a)
     matrixb = np.matrix(b)
-    return np.cross(matrixa,matrixb);
+    return np.dot(matrixa, matrixb);
+
+
+def numpycross(a, b):
+    matrixa = np.matrix(a)
+    matrixb = np.matrix(b)
+    return np.cross(matrixa, matrixb);
+
 
 def numpydet(a):
     return np.linalg.det(a)
+
 
 def solvegaussian(a):
     array = np.array(a)
     pl, u = sp.linalg.lu(array, permute_l=True)
     return u
 
-def generatearray(x,y):
-    return np.random.randint(1,100,(x,y));
+
+def generatearray(x, y):
+    return np.random.randint(1, 100, (x, y))
 
 
-def checkifvalidcrossproduct(a,b):
+'''
+    returns a list where the first value states if its a valid product or not, and the 2nd value is the matrix is its valid.
+'''
+def checkifvalidcrossproduct(a, b):
     try:
         ret = numpycross(a, b)
-        print("its a valid crossproduct")
+        # print("its a valid cross product")
         return [True, ret]
     except:
-        print("its invalid crossproduct ")
+        # print("its invalid cross product ")
         return [False]
 
-def checkifvalidproduct(a,b):
-    try:
-        ret = numpycross(a, b)
-        print("its a valid product")
-        return [True, ret]
-    except:
-        print("its invalid product ")
-        return [False]
 
-def checkifvaliddotproduct(a,b):
+def checkifvaliddotproduct(a, b):
     try:
         ret = numpydot(a, b)
-        print("its a valid dotproduct")
+        # print("its a valid dot product")
         return [True, ret]
     except:
-        print("its invalid dotproduct ")
+        # print("its invalid dot product ")
         return [False]
 
-def checkifvalidmatrixmatrixproduct(a,b):
-    return checkifvaliddotproduct(a,b)
 
-
+def checkifvalidmatrixmatrixproduct(a, b):
+    return checkifvaliddotproduct(a, b)
